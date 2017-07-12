ef3f417e987e2de19ff68d3c745770311ea9d3c2
kgreen14 <kgreen14@gmail.com>
2017-07-12 18:23:48 -0400

    added on click listeners to all buttons and tested functionality with console.log

--- Public/javascript/app.js
+++ Public/javascript/app.js
@@ -3,3 +3,14 @@ window.onload = function()	{
 
 }
 
+
+// Button Event Listeners
+$("#newGame").click(function(){
+    console.log("New Game Button Clicked.");
+});
+$("#hit").click(function(){
+    console.log("Hit Button Clicked.");
+});
+$("#stand").click(function(){
+    console.log("Stand Button Clicked.");
+});
