ab2919ae1836fc68ea4ad0d3a8d585090cc8e71a
kgreen14 <kgreen14@gmail.com>
2017-07-13 15:40:20 -0400

    added playerHand and dealerHand arrays to hold cards dealt to them, and created two functions to pick cards from deck array and push to player/dealer hand arrays.

--- Public/javascript/app.js
+++ Public/javascript/app.js
@@ -1,8 +1,9 @@
 $(function(){
 
-// Game Setup
+var playerHand = [];
+var dealerHand = [];
 
-
+// Deck Setup
 const GameData = {
 	deck: [],
 	buildDeck: function() {
@@ -21,20 +22,69 @@ GameData.buildDeck();
 // console.log(GameData.deck);
 
 
-// Deal a random card
-function dealNewGame() {
+// Deal a new hand
+function dealNewPlayerHand() {
 	var dealCards = GameData.deck[Math.floor(Math.random() * GameData.deck.length)];
-	console.log(dealCards);
+	console.log("dealNewPlayerHand is working");
+	for (var i = 0; i <= 1; i++) {
+		playerHand.push(GameData.deck[Math.floor(Math.random() * GameData.deck.length)]);
+	}
+	
+	return dealCards;
+	
+
+	
 }
-dealNewGame();
+
+function dealNewDealerHand() {
+	var dealCards = GameData.deck[Math.floor(Math.random() * GameData.deck.length)];
+	console.log("dealNewDealerHand is working");
+	for (var i = 0; i <= 1; i++) {
+		dealerHand.push(GameData.deck[Math.floor(Math.random() * GameData.deck.length)]);
+	}
+	
+	return dealCards;
+	
+
+	
+}
+
+
+// User, dealer hands, deal hand function, hit function to check for 21, stand function, check for blackjack or check for win
+
+
+
+// functions new game, hit, stand
+
+function newGameReset() {
+	dealNewPlayerHand();
+	dealNewDealerHand();
+	console.log("dealer hand is " + dealerHand);
+	console.log("player hand is " + playerHand);
+
+}
+
+function hit(){
+// checking total value of cards against other 21
+}
+
+function winOrLose() {
+//  checking total value of cards to see win or lose vs dealerhand
+}
+
+function stand() {
+	// 
+}
+
+// Generate card function: pulling from the player/dealer hands and creating the dom version of the card/
+function createCardsOnBoard() {
+	
+}
 
 
 
 
 
-
-
-// // Game Logic
 
 
 
@@ -43,14 +93,17 @@ dealNewGame();
 // // Button Event Listeners
 $("#newGame").click(function(){
     console.log("New Game Button Clicked.");
+    newGameReset();
     // call  hit function here
 });
 $("#hit").click(function(){
     console.log("Hit Button Clicked.");
-    // call  hit function here
+    hit();
 });
 $("#stand").click(function(){
     console.log("Stand Button Clicked.");
  });
     // call stand function here
 });
+
+var randomRating = (Math.random()* 10);
