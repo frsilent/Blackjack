5ec96a463f30719e7e7328cffc7d079548371b3f
kgreen14 <kgreen14@gmail.com>
2017-07-15 16:20:26 -0400

    added all card images to image dir, added code to append image to DOM to reflect the proper card in playerHand/dealerHand. Used reduce to sum card values of player/dealer when player hits or decides to stand and dealer is hit a card.

--- Public/javascript/app.js
+++ Public/javascript/app.js
@@ -7,7 +7,7 @@ $(function(){
 
 // }
 
-// Need to build card values and link images and store them
+// Need to build card values and cardLink images and store them
 
 var playerHand = [];
 var dealerHand = [];
@@ -19,17 +19,17 @@ var dealerHand = [];
 var GameData = {
 	deck: [],
 	buildDeck: function() {
-		var names = ['2', '3', '4', '5', '6', '7', '8', '9', '10', '10 J', '10 Q', '10 K', '11 A'];
-		var suits = ['Hearts','Diamonds','Spades','Clubs'];
+		var valueOfCard = ['2', '3', '4', '5', '6', '7', '8', '9', '10', '10jack', '10queen', '10king', '11ace'];
+		var suits = ['hearts','diamonds','spades','clubs'];
 	    
-	    for( var n = 0; n < names.length; n++ ) {
+	    for( var n = 0; n < valueOfCard.length; n++ ) {
 	        for( var s = 0; s < suits.length; s++ ) {
-	            // this.deck.push(names[n] + " " + suits[s]);
+	            // this.deck.push(valueOfCard[n] + " " + suits[s]);
 	            GameData.deck.push({
-	            	name: parseInt(names[n]),
+	            	value: parseInt(valueOfCard[n]),
 	            	suit: suits[s],
 	            	player: null,
-	            	image: null,
+	            	image: ("images/" + parseInt(valueOfCard[n]) + "_" + suits[s] + ".jpg"),
 	            });
 	        }
 	    }
@@ -43,6 +43,7 @@ GameData.buildDeck();
 
 
 
+
 // Deal a new hand
 function dealNewPlayerHand() {
 	console.log('dealNewPlayerHand function is working');
@@ -50,8 +51,37 @@ function dealNewPlayerHand() {
 		playerHand.push(GameData.deck[Math.floor(Math.random() * GameData.deck.length)]);
 		// playerHandValue.push(parseInt(playerHand[i]));
 	}
+	var buildCardImage1 = playerHand[0].image;
+	// for (var i = 0; i <= 1; i++) {
+		var cardLink = document.createElement("a");
+		//cardLink.appendChild(document.createTextNode("Anchor"));
+		cardLink.href = "http://stackoverflow.com/questions/7932759/dom-appendchild-to-insert-images/7932803";
+		cardLink.alt = "playerHand.image isnt linking to card";
+
+
+		var img = document.createElement("img");
+		img.src = buildCardImage1;
+		cardLink.appendChild(img);
+		document.getElementById("playerCardsDiv").appendChild(cardLink);
+	// }
+	var buildCardImage2 = playerHand[1].image;
+	// for (var i = 0; i <= 1; i++) {
+		var cardLink = document.createElement("a");
+		//cardLink.appendChild(document.createTextNode("Anchor"));
+		cardLink.href = "http://stackoverflow.com/questions/7932759/dom-appendchild-to-insert-images/7932803";
+		cardLink.alt = "playerHand.image isnt linking to card";
+
+
+		var img = document.createElement("img");
+		img.src = buildCardImage2;
+		cardLink.appendChild(img);
+		document.getElementById("playerCardsDiv").appendChild(cardLink);
+	// }
+	
+	
 	console.log(playerHand);
 	console.log("these are the value of the cards in playerHand")
+	console.log("BUILDING CARD IMAGE " + buildCardImage1 + " " + buildCardImage2);
 	
 }
 
@@ -61,39 +91,94 @@ function dealNewDealerHand() {
 		dealerHand.push(GameData.deck[Math.floor(Math.random() * GameData.deck.length)]);
 		// dealerHandValue.push(parseInt(dealerHand[i]));
 	}
+	var buildCardImage1 = dealerHand[0].image;
+	// for (var i = 0; i <= 1; i++) {
+		var cardLink = document.createElement("a");
+		//cardLink.appendChild(document.createTextNode("Anchor"));
+		cardLink.href = "http://stackoverflow.com/questions/7932759/dom-appendchild-to-insert-images/7932803";
+		cardLink.alt = "playerHand.image isnt linking to card";
+
+
+		var img = document.createElement("img");
+		img.src = buildCardImage1;
+			
+		cardLink.appendChild(img);
+		document.getElementById("dealerCardsDiv").appendChild(cardLink);
+	// }
+	var buildCardImage2 = dealerHand[1].image;
+	// for (var i = 0; i <= 1; i++) {
+		var cardLink = document.createElement("a");
+		//cardLink.appendChild(document.createTextNode("Anchor"));
+		cardLink.href = "http://stackoverflow.com/questions/7932759/dom-appendchild-to-insert-images/7932803";
+		cardLink.alt = "playerHand.image isnt linking to card";
+
+
+		var img = document.createElement("img");
+		img.src = buildCardImage2;
+		
+		cardLink.appendChild(img);
+		document.getElementById("dealerCardsDiv").appendChild(cardLink);
+	// }
 	console.log(dealerHand);
 	console.log("these are the value of the cards in dealerHand")
 	
 }
 
 function hitPlayer() {
-	// var dealCards = GameData.deck[Math.floor(Math.random() * GameData.deck.length)];
+	
 		console.log("hitPlayer function is working via hit button click");
 		playerHand.push(GameData.deck[Math.floor(Math.random() * GameData.deck.length)]);
-		// playerHandValue.push(parseInt(dealCards));
 		
-		// console.log(playerHandValue);
-		console.log("hitPlayer picked this hit card " + playerHand[2].name + " " + playerHand[2].suit)
+		var buildCardImage2 = playerHand[2].image;
 	
+		var cardLink = document.createElement("a");
+		
+		cardLink.href = "https://code.google.com/archive/p/vector-playing-cards/";
+		cardLink.alt = "playerHand.image isnt linking to card";
+
+
+		var img = document.createElement("img");
+		img.src = buildCardImage2;
+		
+		cardLink.appendChild(img);
+		document.getElementById("playerCardsDiv").appendChild(cardLink);
+	
+		console.log(playerHand);
+		console.log("these are the value of the cards in playerHand")
+	
+		total = playerHand.reduce( function(cnt,o){ return cnt + o.value; }, 0);
+
+		console.log("THIS IS THE PLAYER SUM " + total);
 
 	
 }
 function hitDealer() {
-	// var dealCards = GameData.deck[Math.floor(Math.random() * GameData.deck.length)];
+	
 		console.log("hitDealer function is working via stand button click");
-		dealerHand.push(GameData.deck[Math.floor(Math.random() * GameData.deck.length)]);
-		// dealerHandValue.push(parseInt(dealCards));
 		
-		console.log("hitDealer picked this hit card " + dealerHand[2].name + " " + dealerHand[2].suit);
-		// console.log("hit card is " + dealCards)
+		dealerHand.push(GameData.deck[Math.floor(Math.random() * GameData.deck.length)]);
+		
+		console.log("hitDealer picked this hit card " + dealerHand[2].value + " " + dealerHand[2].suit);
+		
+		var buildCardImage2 = dealerHand[2].image;
 	
+		var cardLink = document.createElement("a");
+			cardLink.href = "https://code.google.com/archive/p/vector-playing-cards/";
+			cardLink.alt = "playerHand.image isnt linking to card";
+		var img = document.createElement("img");
+			img.src = buildCardImage2;
+			cardLink.appendChild(img);
+		
+		document.getElementById("dealerCardsDiv").appendChild(cardLink);
+		
+		total = dealerHand.reduce( function(cnt,o){ return cnt + o.value; }, 0);
 
-	
+		console.log("THIS IS THE DEALER SUM " + total);
 }
 function sumPlayerHandValue(array) {
 	console.log("sumPlayerHandValue working")
 	var sumPlayer = 0;
-	for (var i = 0; i < playerHandValue.length; i++) {
+	for (var i = 0; i < playerHand.length; i++) {
 	
 	sumPlayer = sumPlayer + array[i];
 
@@ -102,10 +187,14 @@ function sumPlayerHandValue(array) {
 	
 	console.log("Player total is: " + sumPlayer);	
 }
+
+
+  
+
 function sumDealerHandValue(array) {
 	console.log("sumDealerHandValue working")
 	var sumDealer = 0;
-	for (var i = 0; i < dealerHandValue.length; i++) {
+	for (var i = 0; i < dealerHand[i].value.length; i++) {
 	
 	sumDealer = sumDealer + array[i];
 	
@@ -128,19 +217,19 @@ function newGameDeal() {
 	dealNewDealerHand();
 	console.log("dealer hand is " + dealerHand);
 	console.log("player hand is " + playerHand);
-	// alert("You have " + playerHand + " Dealer has " + dealerHand);
+	
 	
 
 }
 
 function hit(){
-// 	sumPlayerHandValue(playerHandValue);
+
 // 	if (playerHandValue <= 21) {
 	
 
 // }
 hitPlayer();
-console.log(playerHand[2].name + " " + playerHand[2].suit);
+console.log("This is the player's hit card " + playerHand[2].value + " " + playerHand[2].suit);
 
 // then checking total value of cards against > 21 = bust	
 }
@@ -152,14 +241,14 @@ function winOrLose() {
 
 function stand() {
 	// will hit dealer till >=17, if >21 dealer loses
-	// sumDealerHandValue(dealerHandValue);
+	
 	// while (dealerHandValue < 17) {
-	// 	hitDealer();
+	
 	// }
 	console.log("stand button calling stand function working")
 	hitDealer();
-	// sumPlayerHandValue(playerHandValue);
-	// sumDealerHandValue(dealerHandValue);
+	
+	
 
 }
 
